1d765fb0b47b4cec5fcd42f5dcaf3b0991d81910
name <you@example.com>
2022-11-28 14:24:08 +0600

    switch to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SignIn from './components/SignIn';
 import GlobalStyle from './globalStyles';
