ae718a55900f57db82a12ed461adab361cbe27e0
name <you@example.com>
2022-11-28 14:19:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import SignIn from './components/SignIn';
+import GlobalStyle from './globalStyles';
+import Home from './pages';
+import { UserAuthContextProvider } from './contexts/AuthContext'
+import AfterLogin from './pages/AfterLogin';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <UserAuthContextProvider>
+        <GlobalStyle />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path='/home' element={<AfterLogin />} />
+        </Routes>
+      </UserAuthContextProvider>
+    </Router>
   );
 }
 
